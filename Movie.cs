c5063bcce0f6c914a9e0fc9ea5f4e5c91db6e0cf
Jim <jbenkejr@gmail.com>
2019-10-25 08:43:35 -0400

    Movie List

---
+++ Movie.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieList
+{
+    class Movie
+    {
+        //fields
+        private string title;
+        private string category;
+
+        //properties
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        //methods
+        public Movie()
+        {
+
+        }
+
+        public Movie(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+
+        public static List<string> GetMovieCategory(List<Movie> movies)
+        {
+            List<string> categoryTypes = new List<string>();
+            foreach (Movie movie in movies)
+            {
+                if (!categoryTypes.Contains(movie.Category))
+                {
+                    categoryTypes.Add(movie.Category);
+                }
+            }
+
+            return categoryTypes;
+        }
+
+        //public static void PrintMovieCategory(List<Movie> movies)
+        //{
+        //    List<string> movieCategory = Movie.GetMovieCategory(movies);
+        //    for (int i = 0; i < movieCategory.Count; i++)
+        //    {
+        //        Console.Write($"{movieCategory[i]} ");
+        //    } 
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goAgain = true;
+
+            List<Movie> movieTitles = new List<Movie> 
+            {
+                new Movie("Finding Nemo", "Animated"),
+                new Movie("Shrek", "Animated"),
+                new Movie("Raw", "Horror"),
+                new Movie("Halloween", "Horror"),
+                new Movie("Evil Dead", "Horror"),
+                new Movie("Star Wars", "Scifi"),
+                new Movie("Guardians of the Galaxy", "Scifi"),
+                new Movie("Men in Black", "Scifi"),
+                new Movie("The Godfather", "Drama"),
+                new Movie("Baby Driver", "Drama")
+            };
+
+            while (goAgain)
+            {
+                Console.Write("What kind of movie do you want to watch? (Pick a category. Animated, Horror, Scifi or Drama): ");
+                string userInput = Console.ReadLine().ToLower();
+                Console.WriteLine();
+
+                Console.WriteLine($"Here are your movie choices:");
+
+                for (int i = 0; i < movieTitles.Count; i++)
+                {
+                    if (userInput == movieTitles[i].Category.ToLower())
+                    {
+                        Console.WriteLine($"\t{movieTitles[i].Category}\n");
+                    }
+                }
+
+                Console.Write("Would you like to pick another category? (y/n): ");
+                string repeat = Console.ReadLine().ToLower();
+                Console.WriteLine();
+
+                if(repeat == "n")
+                {
+                    goAgain = false;
+                }
+            }
+        }
+    }
+}
